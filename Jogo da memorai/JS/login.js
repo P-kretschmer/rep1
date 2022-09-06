d4f70bcd0c5425179c0aea7f0a12f002459d68db
P-kretschmer <107073206+P-kretschmer@users.noreply.github.com>
2022-09-06 12:28:04 -0400

    Add files via upload

---
+++ Jogo da memorai/JS/login.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+
+const validateInput = ({ target }) => {
+        if (target.value.length >= 3) {
+            button.removeAttribute('disabled');
+            return;
+        }
+        button.setAttribute('disabled', '');
+}
+
+input.addEventListener('input', validateInput);
